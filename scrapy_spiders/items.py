83dfbb1c1649de4cbae867e93ef81b02e474e7c7
yunfeiroy666 <yunfeiroy@gmail.com>
2017-02-14 23:23:40 +0800

    start scrapy-spiders and add zhuanziban

---
+++ scrapy_spiders/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZhuanzibanCinemaItem(scrapy.Item):
+    date = scrapy.Field()
+    cinema_company = scrapy.Field()
+    daily_box_office = scrapy.Field()
+    action_cutting = scrapy.Field()
+    daily_viewers = scrapy.Field()
+    counter_sales = scrapy.Field()
+    counter_sales_percent = scrapy.Field()
+    online_sales = scrapy.Field()
+    online_sales_percent = scrapy.Field()
+
---
+++ scrapy_spiders/spiders/zhuanziban_cinema_info.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8-*-
+import datetime
+
+import scrapy
+from scrapy.selector import Selector
+
+from scrapy_spiders.items import ZhuanzibanCinemaItem
+
+
+class ZhuanzibanCinemaSpier(scrapy.Spider):
+    name = 'zhuanziban_cinema'
+    allowed_domains = ['http://111.205.151.7/cine_groups/']
+    # start_urls = ['http://111.205.151.7/cine_groups/{0}-{1}-{2}'.format(datetime.datetime.now().year,
+    #                                                                     datetime.datetime.now().month,
+    #                                                                     datetime.datetime.now().day)]
+    start_urls=['http://111.205.151.7/cine_groups/2017-01-01']
+    # custom_settings = {
+    #     'SCHEDULER': "scrapy.core.scheduler.Scheduler",
+    #     'ITEM_PIPELINES': {
+    #         'crawler.pipelines.zhuanziban_info_redshift_pipeline.ZhuanzibanCinemaPipeline': 400
+    #     }
+    # }
+
+    def parse(self, response):
+        html = response.body.decode()
+        selector = Selector(text=html)
+        movie_group = selector.xpath('//html/body/div')
+        for movie_item in movie_group:
+            for i in range(1, 11):  # Information in a table with ten rows.
+                item = ZhuanzibanCinemaItem()
+                item['date'] = movie_group.xpath(
+                    'div[1]/header/a[2]/span[1]/text()').extract_first()
+                item['cinema_company'] = movie_item.xpath('table/tbody/tr[{0}]/td[1]/text()'.format(i)).extract_first()
+                item['daily_box_office'] = int(movie_item.xpath(
+                    'table/tbody/tr[{0}]/td[2]/text()'.format(i)).extract_first().replace(',', ''))
+                item['action_cutting'] = int(movie_item.xpath(
+                    'table/tbody/tr[{0}]/td[3]/text()'.format(i)).extract_first().replace(',', '').replace(',',
+                                                                                                           '').replace(
+                    '场', ''))
+                item['daily_viewers'] = int(
+                    movie_item.xpath('table/tbody/tr[{0}]/td[3]/i/text()'.format(i)).extract_first().replace(',', ''))
+                item['counter_sales'] = int(
+                    movie_item.xpath('table/tbody/tr[{0}]/td[4]/text()'.format(i)).extract_first().replace(',', ''))
+                item['counter_sales_percent'] = movie_item.xpath(
+                    'table/tbody/tr[{0}]/td[4]/i/text()'.format(i)).extract_first()
+                item['online_sales'] = int(
+                    movie_item.xpath('table/tbody/tr[{0}]/td[5]/text()'.format(i)).extract_first().replace(',', ''))
+                item['online_sales_percent'] = movie_item.xpath(
+                    'table/tbody/tr[{0}]/td[5]/i/text()'.format(i)).extract_first()
+                # yield item
+                print(item)
