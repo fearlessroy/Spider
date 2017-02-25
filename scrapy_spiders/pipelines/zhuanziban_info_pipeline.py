a27c383a497fff93ca113302b2437d68dafec633
yunfeiroy666 <yunfeiroy@gmail.com>
2017-02-25 19:41:04 +0800

    add zhuanziban_pipeline

---
+++ scrapy_spiders/pipelines/zhuanziban_info_pipeline.py
@@ -0,0 +1,17 @@
+from crawler.pipelines.base_pipeline import BasePipeline
+from orm.models.zhuanziban import ZhuanzibanCinema
+
+
+class ZhuanzibanCinemaPipeline(BasePipeline):
+    def __init__(self):
+        super().__init__(batch_process_number=1, use_mysql=False,
+                         jarvis_logger_filename="zhuanziban_cinema_pipeline.log")
+
+    def process_item(self, item, spider):
+        try:
+            cinemaitem = ZhuanzibanCinema(**item)
+            self.batch.append(cinemaitem)
+
+        except Exception as e:
+            self.jarvis_logger.exception(e=e)
+
