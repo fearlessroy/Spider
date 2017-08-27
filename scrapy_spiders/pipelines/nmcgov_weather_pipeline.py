6f8c86d97cac9cdd62b209a3c69183db2c3d2d58
yunfeiroy666 <yunfeiroy@gmail.com>
2017-08-27 10:41:03 +0800

    modify nmc_weather_pipeline

--- scrapy_spiders/pipelines/nmcgov_weather_pipeline.py
+++ scrapy_spiders/pipelines/nmcgov_weather_pipeline.py
@@ -1,32 +1,19 @@
-import sys
-
-from .base_pipeline import BasePipeline
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import pymongo
 
 
-# class NmcgovWeatherPipeline(object):
-#     def __init__(self):
-#         self.connection = pymongo.MongoClient('127.0.0.1', 27017)
-#         self.tdb = self.connection.foobar
-#         self.post = self.tdb.weather
-#         # self.file = open('weather_item.txt', 'wb')
-#
-#     def process_item(self, item, spider):
-#         # line = json.dumps(dict(item), ensure_ascii=False)
-#         # self.file.write(line)
-#         self.post.insert(dict(item))
-#         return item
-#
-#     def close_spider(self, spider):
-#         # self.file.close()
-#         self.connection.close()
-
-class NmcgovWeatherPipeline(BasePipeline):
+class NmcgovWeatherPipeline(object):
     def __init__(self):
-        super(NmcgovWeatherPipeline, self).__init__(mongo_db='foobar', mongo_collection='weather')
+        self.connection = pymongo.MongoClient('127.0.0.1', 27017)
+        self.tdb = self.connection.nmc_weather
+        self.post = self.tdb.weather
+        # self.file = open('weather_item.txt', 'wb')
 
     def process_item(self, item, spider):
+        # line = json.dumps(dict(item), ensure_ascii=False)
+        # self.file.write(line)
         self.post.insert(dict(item))
         return item
+
+    def close_spider(self, spider):
+        # self.file.close()
+        self.connection.close()
