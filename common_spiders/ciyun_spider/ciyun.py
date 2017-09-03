5201f9b8e8e83caa3fce0202a34bfff52f8f84ce
yunfeiroy666 <yunfeiroy@gmail.com>
2017-09-03 16:36:55 +0800

    modify ciyun add font_path

--- common_spiders/ciyun_spider/ciyun.py
+++ common_spiders/ciyun_spider/ciyun.py
@@ -9,12 +9,12 @@ import jieba
 形成关键词的渲染形成类似云一样的彩色图片，从而一眼就可以领略文本数据的主要表达意思。
 '''
 
-text_from_file_with_apath = open('', encoding='utf-8').read()
+text_from_file_with_apath = open('test.txt', encoding='utf-8').read()
 
 wordlist_after_jieba = jieba.cut(text_from_file_with_apath, cut_all=True)
 wl_space_split = " ".join(wordlist_after_jieba)
 
-my_wordcloud = WordCloud().generate(wl_space_split)
+my_wordcloud = WordCloud(font_path='MSYH.TTC').generate(wl_space_split)
 
 plt.imshow(my_wordcloud)
 plt.axis("off")
